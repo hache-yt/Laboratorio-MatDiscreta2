3901a52afa90fe959a304bc936eeb91bae3e3e41
hache-yt <62501501@continental.edu.pe>
2025-11-05 11:17:51 -0500

    Add 'XD' to greeting message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def saludo(nombre: str) -> str:
-    return f"Hola, {nombre}! Bienvenido a Git, commit exitoso."
+    return f"Hola, {nombre}! Bienvenido a Git, commit exitoso, XD."
 
 if __name__ == "__main__":
     nombre = input("¿Tu nombre? ")
